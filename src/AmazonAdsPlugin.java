80f4f70b24e4755129cc168c6afdf8d3ff25a56d
QuantumRand <QuantumRand@users.noreply.github.com>
2014-06-27 03:25:18 -0700

    Update AmazonAdsPlugin.java

--- src/AmazonAdsPlugin.java
+++ src/AmazonAdsPlugin.java
@@ -6,7 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
 
 import com.amazon.device.ads.AdLayout;
 import com.amazon.device.ads.AdRegistration;
@@ -19,46 +19,63 @@ public class AmazonAdsPlugin extends CordovaPlugin{
 	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		try {
-		    if (ACTION_DISPLAY_AMAZON_ADS.equals(action)) {
-		    	
-		    	JSONObject arg_object = args.getJSONObject(0);
-		    	displayAmazonAds(arg_object);
-		    	callbackContext.success();
-		    	return true;
-		    }
-		    callbackContext.error("Invalid action");
+		
+		if (ACTION_DISPLAY_AMAZON_ADS.equals(action)) {
+			JSONObject arg_object;
+			arg_object = args.getJSONObject(0);
+			displayAmazonAds(arg_object, callbackContext);
+			return true;
+		}
+		else {
+			callbackContext.error("Invalid action");
 		    return false;
-		} catch(Exception e) {
-		    System.err.println("Exception: " + e.getMessage());
-		    callbackContext.error(e.getMessage());
-		    return false;
-		} 
+		}
+		
 	}
 	
-	private void displayAmazonAds(JSONObject args_object) throws JSONException{
+	private void displayAmazonAds(JSONObject args_object, final CallbackContext callbackContext){
+		
+		
 		adView = (AdLayout) new AdLayout(this.cordova.getActivity());
-		boolean debug = args_object.getBoolean("debug");
-		boolean isOnTop = args_object.getBoolean("isOnTop");
-		String adKey = args_object.getString("adKey");
+		final boolean debug;
+		final boolean isOnTop;
+		final String appKey;
+		try {
+			debug = args_object.getBoolean("debug");
+			isOnTop = args_object.getBoolean("isOnTop");
+			appKey = args_object.getString("appKey");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			callbackContext.error(e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+		cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+            	
+            
+				// For debugging purposes enable logging, but disable for production builds.
+		        AdRegistration.enableLogging(debug);
+		        // For debugging purposes flag all ad requests as tests, but set to false for production builds.
+		        AdRegistration.enableTesting(debug);
+		        // Register Application Key
+		        AdRegistration.setAppKey(appKey);
+		        
+		        if (adView.getParent() != null) {							//check for existing ad
+		            ((ViewGroup)adView.getParent()).removeView(adView); 
+		        }
+		        ViewGroup parentView = (ViewGroup) webView.getParent();
+		        if (isOnTop) {
+		            parentView.addView(adView, 0);
+		        } else {
+		            parentView.addView(adView);
+		        }
+		        
+		        adView.loadAd();
+		        callbackContext.success(); // Thread-safe.
+		        
+            }
 		
-		// For debugging purposes enable logging, but disable for production builds.
-        AdRegistration.enableLogging(debug);
-        // For debugging purposes flag all ad requests as tests, but set to false for production builds.
-        AdRegistration.enableTesting(debug);
-        // Register Application Key
-        AdRegistration.setAppKey(adKey);
-        
-        LinearLayout layout = new LinearLayout(this.cordova.getActivity());
-        
-        if(isOnTop)
-        	layout.addView(adView);
-        else
-        	layout.addView(adView, layout.getChildCount());
-        
-        adView.loadAd();
-		
-		
-		
+		});
 	}
 }
